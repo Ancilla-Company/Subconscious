9419731413aa42a24f42b470fd25a4989f1e0a5e
Brian Branch <b.branch@ancilla.company>
2026-04-22 08:01:57 +0200

    Update to github workflow

--- src/flet_ep.py
+++
@@ -1,9 +0,0 @@
-""" Default flet build entrypoint """
-import sys
-from subconscious.cli import main
-
-
-if __name__ == "__main__":
-  if "gui" not in sys.argv:
-    sys.argv.insert(1, "gui")
-  main()
